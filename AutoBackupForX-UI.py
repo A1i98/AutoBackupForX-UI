c38b6de405b83a57f18b09ab6ea953c69f373877
Ali <53361525+A1i98@users.noreply.github.com>
2023-04-13 01:35:31 +0330

    Update AutoBackupForX-UI.py

--- AutoBackupForX-UI.py
+++ AutoBackupForX-UI.py
@@ -6,7 +6,7 @@ import asyncio
 
 # Replace with your bot token and channel name
 bot_token = "your bot token here"
-channel_name = "@your channel name here"
+channel = "@your channel name here or id"
 
 # Create a bot instance
 bot = telegram.Bot(token=bot_token)
@@ -19,13 +19,13 @@ file_path = os.path.join(os.getcwd(), file_name)
 async def send_file():
     # Create a loop to send the file every hour
     while True:
-        print(f"Sending {file_path} to {channel_name}...")
+        print(f"Sending {file_path} to {channel}...")
 
         # Open the file as a binary stream
         with open(file_path, "rb") as file:
             # Send the file as a document
-            bot.send_document(
-                chat_id=channel_name,
+            await bot.send_document(
+                chat_id=channel,
                 document=file,
                 filename=file_name
             )
@@ -33,7 +33,9 @@ async def send_file():
         print("File sent successfully.")
 
         # Wait for an hour (3600 seconds)
-        time.sleep(3600)
+        await time.sleep(3600)
 
-# Call the async function with asyncio.run
-asyncio.run(send_file())
+if name == 'main': 
+    loop = asyncio.new_event_loop() 
+    asyncio.set_event_loop(loop) 
+    loop.run_until_complete(send_file())
